8481801b335bd9e293aa2724ee5f9f94cb997586
Aarón Maldonado <sekamdex@gmail.com>
2022-05-23 14:14:00 -0500

    updated prettier config file

--- packages/config/prettier-preset.js
+++ packages/config/prettier-preset.js
@@ -13,7 +13,7 @@ module.exports = {
 
   // Print semicolons at the end of statements
   // https://prettier.io/docs/en/options.html#semicolons
-  semi: true, 
+  semi: true,
 
   // Use single quotes
   // https://prettier.io/docs/en/options.html#quotes
@@ -21,7 +21,7 @@ module.exports = {
 
   // Trailing commas where valid in ES5 (objects, arrays, etc.). No trailing commas in type parameters in TypeScript.
   // https://prettier.io/docs/en/options.html#trailing-commas
-  trailingComma: 'es5',
+  trailingComma: "es5",
 
   // Print spaces between brackets in object literals
   //https://prettier.io/docs/en/options.html#bracket-spacing
@@ -30,13 +30,13 @@ module.exports = {
 
   // Put the > of a multi-line HTML element at the end of the last line
   // https://prettier.io/docs/en/options.html#bracket-line
-  bracketSameLine: false, 
+  bracketSameLine: false,
 
   // Include parentheses around a sole arrow function parameter
   // https://prettier.io/docs/en/options.html#arrow-function-parentheses
-  arrowParens: 'always',
+  arrowParens: "always",
 
   // Maintain existing line endings
   // https://prettier.io/docs/en/options.html#end-of-line
-  endOfLine: 'auto',
+  endOfLine: "auto",
 };
