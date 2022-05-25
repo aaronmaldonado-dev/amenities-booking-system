0d89a771555a88bdfec66b6c7169aec0b8450c7b
Aarón Maldonado <sekamdex@gmail.com>
2022-05-25 17:22:05 -0500

    added basic users crud

--- apps/api/src/resources/user/user.controller.ts
+++ apps/api/src/resources/user/user.controller.ts
@@ -1,1 +1,7 @@
-export const userController;
+import { deleteOne, getAll, getOne, updateOne } from '../../utils/handlerFactory';
+import { User } from '../user/user.model';
+
+export const updateOneUser = updateOne(User);
+export const deleteOneUser = deleteOne(User);
+export const getAllUsers = getAll(User);
+export const getOneUser = getOne(User);
--- apps/api/src/resources/user/user.router.ts
+++ apps/api/src/resources/user/user.router.ts
@@ -1,18 +1,16 @@
 import { Router } from 'express';
 
 import { login, signup } from '../auth/auth.controller';
+import { deleteOneUser, getAllUsers, getOneUser, updateOneUser } from './user.controller';
 
 export const router = Router();
 
-router.post('/signup', signup);
 router.post('/login', login);
+router.post('/signup', signup);
+router.route('/').get(getAllUsers);
 
-// router // breakme
-//   .route('/user')
-//   .get(userController.getAllUsers)
-//   .post(userController.createUser);
-
-// router // breakme
-//   .route('/user/:id')
-//   .get(userController.getUser)
-//   .patch(userController.updateUser);
+router //breakme
+  .route('/:id')
+  .get(getOneUser)
+  .delete(deleteOneUser)
+  .patch(updateOneUser);
