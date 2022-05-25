303cfe0ad4fb2638eb2b178dc593ad298e689fb8
Aarón Maldonado <sekamdex@gmail.com>
2022-05-25 10:50:52 -0500

    added constants to server.ts

---
+++ apps/api/src/constants/index.ts
@@ -0,0 +1,5 @@
+export const API_VERSION = '/api/v1';
+
+export const USERS_PATH = '/users';
+
+export const AMENITIES_PATH = '/amenities';
--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -4,7 +4,10 @@ import cors from 'cors';
 import express, { Application } from 'express';
 import mongoose from 'mongoose';
 
+import { AMENITIES_PATH, API_VERSION, USERS_PATH } from './constants';
+import { router as amenityRouter } from './resources/amenity/amenity.router';
 import { router as userRouter } from './resources/user/user.router';
+import { catchAsync } from './utils/catchAsync';
 
 const { DATABASE, PORT } = process.env;
 
@@ -18,15 +21,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/v1/users', userRouter);
+app.use(`${API_VERSION}${USERS_PATH}`, userRouter);
+app.use(`${API_VERSION}${AMENITIES_PATH}`, amenityRouter);
 
-export const start = async () => {
-  try {
-    mongoose.connect(DATABASE);
-    app.listen(PORT, () => {
-      console.log(`REST API on http://localhost:${PORT}/`);
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
+export const start = catchAsync(async () => {
+  mongoose.connect(DATABASE);
+  app.listen(PORT, () => {
+    console.log(`REST API on http://localhost:${PORT}/`);
+  });
+});
